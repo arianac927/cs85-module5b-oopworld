67b6a7fe07d6edc327757a9486246a567c3adcfd
Ariana Castillo-Jara <ariana13castillo@gmail.com>
2026-07-12 19:25:23 -0700

    added 2 methods and constructor

--- MyObject.php
+++ MyObject.php
@@ -2,9 +2,45 @@
 <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <title>My Object</title>
+        <title>Book Tracker</title>
     </head>
     <body>
-        
+        <h1>My Book Tracker</h1>
+        class Book {
+            public int $bookNumber;
+            public string $title;
+            public string $author;
+            public int $totalPages;
+            public string $genre;
+        }
+
+        public function __construct(int $bookNumber, string $title, string $author, int $totalPages, string $genre) {
+            $this->bookNumber = $bookNumber;
+            $this->title = $title;
+            $this->author = $author;
+            $this->totalPages = $totalPages;
+            $this->genre = $genre;
+        }
+
+        $books = [
+            new Book(1, "Shadow and Bone", "Leah Bardugo", 358, "Fantasy"),
+            new Book(2, "The Deal", "Elle Kennedy", 342, "Romance"),
+            new Book(3, "The Midnight Library", "Matt Haig", 288, "Fantasy"),
+            new Book(4, "The List of Suspicious Things", "Jennie Godfrey", 416, "Mystery"),
+            new Book(5, "The Summer I Turned Pretty", "Jenny Han", 288, "Romance")
+        ];
+
+        // Displays all books from array
+            public function displayBooks(array $books) {
+                foreach ($books as $book) {
+                    echo $book . "\n";
+                }
+            }
+
+        // Displays total number of books within array
+            public function countBooks(array $books): int {
+                $total = count($books);
+                return $total;
+            }
     </body>
 </html>
